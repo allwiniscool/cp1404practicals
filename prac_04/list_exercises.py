9706ea12224df713e3e1c1c8a4857c86a0282e13
thoma <thomasallwin700@gmail.com>
2025-10-12 16:13:56 +1000

    Fully complete list_exercises.py.

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,14 +1,19 @@
-def main ():
-    numbers = []
-    for i in range(0, 5):
-        input_number = int(input("Number: "))
-        numbers.append(input_number)
-    print(f"The first number is {numbers[0]}")
-    print(f"The last number is {numbers[-1]}")
-    print(f"The smallest number is {min(numbers)}")
-    print(f"The largest number is {max(numbers)}")
-    average = sum(numbers)/len(numbers)
-    print(f"The average of the numbers is {average:.2f}")
 
-main()
+numbers = []
+for i in range(0, 5):
+    input_number = int(input("Number: "))
+    numbers.append(input_number)
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+average = sum(numbers)/len(numbers)
+print(f"The average of the numbers is {average:.2f}")
 
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Username: ")
+if username in usernames:
+    print("access granted")
+else:
+    print("access denied")
