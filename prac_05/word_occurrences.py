a88d0e7b35ed99d2c42c23a7c16b796af181ae82
thoma <thomasallwin700@gmail.com>
2025-10-19 22:59:25 +1000

    Complete word_occurrences.py

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,18 @@
+"""
+Word Occurrences
+Estimate: 20 minutes
+Actual: 30 minutes
+"""
+word_count = {}
+text = input("Text: ").split()
+for word in text:
+    if word in word_count:
+        word_count[word] += 1
+    else:
+        word_count[word] = 1
+
+max_length = max(len(word) for word in word_count)
+
+for word, count in sorted(word_count.items()):
+    print(f"{word:{max_length}} : {count}")
+
