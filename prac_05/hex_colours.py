669619ecd21dcdfced8f112b9077c34e744d7539
thoma <thomasallwin700@gmail.com>
2025-10-20 23:40:31 +1000

    Complete hex_colours.py

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -3,6 +3,7 @@ COLOUR_TO_CODE = {"AliceBlue": "#f0f8ff", "Amaranth": "#e52b50", "Amber": "#ffbf
                   "AntiqueWhite": "#faebd7", "Aquamarine1": "#7fffd4", "Asparagus": "#87a96b", "Barn Red": "#7c0a02",
                   "Bleu de France": "#318ce7", "Bright Green": "#66ff00"}
 
+
 colour_name = input("Enter a colour name: ").strip()
 while colour_name != "":
     if colour_name.lower() in [key.lower() for key in COLOUR_TO_CODE]:
