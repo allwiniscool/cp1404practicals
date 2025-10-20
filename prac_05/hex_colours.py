149b29380d4c392a7b0f43d9469dcc720f24607c
thoma <thomasallwin700@gmail.com>
2025-10-20 10:29:50 +1000

    Complete hex_colours.py

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -3,13 +3,14 @@ COLOUR_TO_CODE = {"AliceBlue": "#f0f8ff", "Amaranth": "#e52b50", "Amber": "#ffbf
                   "AntiqueWhite": "#faebd7", "Aquamarine1": "#7fffd4", "Asparagus": "#87a96b", "Barn Red": "#7c0a02",
                   "Bleu de France": "#318ce7", "Bright Green": "#66ff00"}
 
-colour_name = input("Enter colour name: ").lower()
-
-
+colour_name = input("Enter a colour name: ").strip()
 while colour_name != "":
-    try:
-        print(colour_name, "is", COLOUR_TO_CODE[colour_name])
-    except KeyError:
-        print("Invalid colour name")
+    if colour_name.lower() in [key.lower() for key in COLOUR_TO_CODE]:
+        for name, code in COLOUR_TO_CODE.items():
+            if colour_name.lower() == name.lower():
+                print(f"The code for {name} is {code}")
+    else:
+        print("Invalid colour name.")
+    colour_name = input("Enter a colour name: ").strip()
 
-    colour_name = input("Enter colour name: ").lower()
+print("Goodbye!")
