a95045a2b3ddef0361cf1c90968e136f24c37332
thoma <thomasallwin700@gmail.com>
2025-11-02 11:26:29 +1000

    Complete guitar.py, guitar_test.py, guitars.py

---
+++ prac_06/guitars.py
@@ -0,0 +1,18 @@
+"""expected: 10
+actual: 16"""
+
+from prac_06.guitar import Guitar
+
+print("My guitars!")
+guitars = []
+name = input("Name: ")
+while name != "":
+    year = int(input("Year: "))
+    cost = float(input("Cost: $"))
+    guitar = Guitar(name, year, cost)
+    guitars.append(guitar)
+    print(f"{guitar} added")
+    name = input("Name: ")
+for i, guitar in enumerate(guitars, 1):
+    vintage_string = "(vintage)" if guitar.is_vintage() else ""
+    print(f"Guitar {i}: {guitar.name:>20} {guitar.year}, worth ${guitar.cost:10.2f}{vintage_string}")
