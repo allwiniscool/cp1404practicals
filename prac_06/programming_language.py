34870a4d252e8d0dd9ecdaefca31f04814e4276f
thoma <thomasallwin700@gmail.com>
2025-11-02 12:45:13 +1000

    Complete languages.py, and programming_language.py

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,14 +1,17 @@
 
 class ProgrammingLanguage:
+    """Represents a ProgrammingLanguage object."""
     def __init__(self, name = "", typing = '', reflection = False, year = 0):
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
     def is_dynamic(self):
+        """Determine if the typing is dynamic."""
         if self.typing == "Dynamic":
             return True
         else:
             return False
     def __str__(self):
+        """Return a string of information."""
         return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
