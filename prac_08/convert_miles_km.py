55e8adec43c79a31f5b475611f02271dd071c12e
allwiniscool <thomasallwin700@gmail.com>
2025-11-16 21:53:35 +1000

    Make changes to convert_miles_km.py

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,26 +1,30 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
-CONVERTOR_FOR_MILES_TO_KM = 1.60934
+CONVERSION_FACTOR_FOR_MILES_TO_KM = 1.60934
 
 
 class Convertor(App):
-    """ SquareNumberApp is a Kivy App for squaring a number """
+    """
+    Main program
+    """
     def build(self):
-        """ build the Kivy app from the kv file """
+        """ Build the Kivy app from the kv file """
         Window.size = (200, 100)
         self.title = " convert miles to km "
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
     def convert_miles_to_km(self):
+        """ convert miles to km """
         try:
-            mile_to_km = float(self.root.ids.input_mile.text) * CONVERTOR_FOR_MILES_TO_KM
+            mile_to_km = float(self.root.ids.input_mile.text) * CONVERSION_FACTOR_FOR_MILES_TO_KM
             self.root.ids.km_output.text = str(mile_to_km)
         except ValueError:
             mile_to_km = 0
             self.root.ids.km_output.text = str(mile_to_km)
     def up_or_down(self, up_or_down = 0):
+        """Change mile by 1 or -1."""
         try:
             mile = float(self.root.ids.input_mile.text) + up_or_down
             self.root.ids.input_mile.text = str(mile)
