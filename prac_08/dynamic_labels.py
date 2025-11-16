fbc3f1ab6db3bec71dbb4a866e3fec1aa99867d0
allwiniscool <thomasallwin700@gmail.com>
2025-11-16 21:52:51 +1000

    Complete dynamic_labels.kv/py

---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,34 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+
+
+
+
+class DynamicWidgetsLabel(App):
+    """
+    Main program
+    """
+    def __init__(self, **kwargs):
+        """
+        Construct main app
+        """
+        super().__init__(**kwargs)
+        self.names= ["Bob Brown", "Cat Cyan", "Oren Ochre"]
+
+    def build(self):
+        """ Build the Kivy app from the kv file """
+        self.title = " convert miles to km "
+        self.root = Builder.load_file("dynamic_labels.kv")
+        self.create_labels()
+        return self.root
+
+    def create_labels(self):
+        """Create labels from names."""
+        for name in self.names:
+            temp_label = Label(text=name)
+            self.root.ids.main.add_widget(temp_label)
+
+
+
+DynamicWidgetsLabel().run()
